62fcb3c2a3ec68470932c7da4f12ac01da2adf09
flashjaysan <jaysan@wanadoo.fr>
2021-05-03 19:36:22 +0200

    day 10

---
+++ 100-days-of-python-challenge/day-010/calculator.py
@@ -0,0 +1,62 @@
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+
+def add(number1, number2):
+    return number1 + number2
+
+
+def subtract(number1, number2):
+    return number1 - number2
+
+
+def multiply(number1, number2):
+    return number1 * number2
+
+
+def divide(number1, number2):
+    return number1 / number2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+
+def calculator():
+    print(logo)
+    number1 = float(input("What's the first number? "))
+    should_continue = True
+    while should_continue:
+        for operator in operations:
+            print(operator)
+        operation_symbol = input("Pick an operation: ")
+        number2 = float(input("What's the next number? "))
+        result = operations[operation_symbol](number1, number2)
+        print(f"{number1} {operation_symbol} {number2} = {result}")
+        continue_answer = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
+        if continue_answer == 'y':
+            number1 = result
+        elif continue_answer == 'n':
+            should_continue = False
+
+
+while True:
+      calculator()
